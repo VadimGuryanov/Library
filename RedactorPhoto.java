69eec40d6eb0e41230117676826f0566bd091ab3
Vadim Guryanov <gvadimmonstors@mail.ru>
2018-12-08 00:02:48 +0300

    asd

--- RedactorPhoto.java
+++ RedactorPhoto.java
@@ -2,91 +2,15 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Random;
 import javax.imageio.ImageIO;
-
-
 
 public class RedactorPhoto {
 
     private int width = 640;
     private int height = 480;
-//    private String nameFile1;
-//    private String nameFile2;
-//    public RedactorPhoto(String a, String b) {
-//        nameFile1 = a;
-//        nameFile2 = b;
-//    }
     BufferedImage picture = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-    public void blackAndWhite(String nameFile1, String nameFile2) {
-        File imagina = new File(nameFile1);
-        try {
-            picture = ImageIO.read(imagina); //new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        }
-        catch(IOException io) {
-            System.out.println(io.getLocalizedMessage());
-        }
-        picture.createGraphics();
-        Graphics2D g2d = picture.createGraphics();
-        for(int i = 0; i < picture.getWidth(); i++) {
-            for (int j = 0; j < picture.getHeight(); j++) {
-                int cl = picture.getRGB(i, j);
-                Color c = new Color(cl);
-                int green = c.getGreen();
-                int blue = c.getBlue();
-                int red = c.getRed();
-                int otvet = (green + red + blue) / 3;
-                Color cn = new Color(otvet, otvet, otvet);
 
-                picture.setRGB(i,j, cn.getRGB());
-            }
-        }
-        g2d.dispose();
-        File file = new File(nameFile2);
-        try {
-            ImageIO.write(picture, "png", file);
-        }
-        catch(IOException io) {
-            System.out.println(io.getLocalizedMessage());
-        }
-    }
-
-    public void makeNegative(String nameFile1, String nameFile2){
-        File imagina = new File(nameFile1);
-        try {
-            picture = ImageIO.read(imagina); //new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        }
-        catch(IOException io) {
-            System.out.println(io.getLocalizedMessage());
-        }
-        picture.createGraphics();
-        Graphics2D g2d = picture.createGraphics();
-        for(int i = 0; i < picture.getWidth(); i++) {
-            for (int j = 0; j < picture.getHeight(); j++) {
-                int cl = picture.getRGB(i, j);
-                Color c = new Color(cl);
-                int green = c.getGreen();
-                int blue = c.getBlue();
-                int red = c.getRed();
-                int otvet = 255 - (green + red + blue) / 3;
-                Color cn = new Color(otvet, otvet, otvet);
-
-                picture.setRGB(i,j, cn.getRGB());
-            }
-        }
-
-        g2d.dispose();
-
-        File file = new File(nameFile2);
-        try {
-            ImageIO.write(picture, "png", file);
-        }
-        catch(IOException io) {
-            System.out.println(io.getLocalizedMessage());
-        }
-    }
-
-    public void YYY(String nameFile1, String nameFile2, int a){
+    public void lighten(String nameFile1, String nameFile2, int a){
         a = (int)((double) a / 100 * 255);
         File imagina = new File(nameFile1);
         try {
@@ -101,29 +25,20 @@ public class RedactorPhoto {
             for (int j = 0; j < picture.getHeight(); j++) {
                 int cl = picture.getRGB(i, j);
                 Color c = new Color(cl);
-                int green = c.getGreen();
-                int blue = c.getBlue();
-                int red = c.getRed();
-                int newGreen = 0;
-                int newBlue = 0;
-                int newRed =  0;
-                if (green + a > 255) {
-                    newGreen = 255;
-                } else {
-                    newGreen = (green + a);
+                int[] color = new int[3];
+                color[0] = c.getGreen();
+                color[1] = c.getBlue();
+                color[2] = c.getRed();
+                for (int m  = 0; m < 3; m++) {
+                    if (color[m] + a < 0) {
+                        color[m] = 0;
+                    } else if (color[m] + a > 255) {
+                        color[m] = 255;
+                    } else {
+                        color[m] = color[m] + a;
+                    }
                 }
-                if (red + a > 255) {
-                    newRed = 255;
-                }
-                else {
-                    newRed = (red + a);
-                }
-                if (blue + a > 255) {
-                    newBlue = 255;
-                } else {
-                    newBlue = (blue + a);
-                }
-                Color cn = new Color(newRed, newGreen , newBlue);
+                Color cn = new Color(color[0], color[1] , color[2]);
 
                 picture.setRGB(i,j, cn.getRGB());
             }
@@ -139,8 +54,5 @@ public class RedactorPhoto {
             System.out.println(io.getLocalizedMessage());
         }
     }
-
-
-
 
 }
